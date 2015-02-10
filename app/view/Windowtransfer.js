37b51cbb675b0169e87e5f9b323b50de3937543d
underflow1 <underflow@mail.ru>
2015-02-11 00:49:01 +0300

    c10
    нарисовал шаблон для приложения 2

--- app/view/Windowtransfer.js
+++ app/view/Windowtransfer.js
@@ -7,6 +7,7 @@ Ext.define('BeeApp.view.Windowtransfer', {
         width : 400,
         title: 'Передать сим карту',
         modal: true,
+        resizable: false,
         items: [{
             //'/testsim/{phonenumber}/givethenumber/{tariff}/{fio}/{position}/{deduction}/{pkg}/{roam}'
             xtype: 'form',
